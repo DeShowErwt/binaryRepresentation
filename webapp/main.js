c247bb76089f058e6bbc0a4a0fbd40f7a9a44757
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-03-15 15:29:33 +0100

    Clearer overview of what happens and dont call displaylist function unnecessarily

--- webapp/main.js
+++ webapp/main.js
@@ -2,10 +2,10 @@ import { binaryElement } from './js/binary.js'
 import { setupCounter } from './js/counter.js'
 
 setupCounter(document.querySelector('.counter'))
+const binary = document.querySelector('.binary')
 let byteList = [1,2,4,8,16,32,64,128]
 let bitList = [1]
 function displayList(chosenList){
-    const binary = document.querySelector('.binary')
     binary.innerHTML = ''
     for(let i = chosenList.length-1; i>=0;i--){
          binary.innerHTML += `
@@ -15,5 +15,13 @@ function displayList(chosenList){
 }
 displayList(bitList)
 // Setup eventListeners for changing the size of the bytes
-document.querySelector('#bitSelect').addEventListener('click', () => {displayList(bitList)})
-document.querySelector('#byteSelect').addEventListener('click', () => {displayList(byteList)})
+document.querySelector('#bitSelect').addEventListener('click', function(){
+    if(document.getElementsByTagName('binary-element').length > 1){
+        displayList(bitList)
+    }
+})
+document.querySelector('#byteSelect').addEventListener('click', function(){
+    if(document.getElementsByTagName('binary-element').length < 2){
+        displayList(byteList)
+    }
+})
