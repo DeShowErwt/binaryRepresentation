0286897e50d0fa3e93e37479d4d293c6bebaced2
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-03-15 11:41:05 +0100

    #1 & #2 Digit values get added works for any amount of bits

--- webapp/js/counter.js
+++ webapp/js/counter.js
@@ -1,10 +1,21 @@
 export function setupCounter(element) {
     let counter = 0
-    const setCounter = (count) => {
-      counter = count
+    const addValue = (count) => {
+      counter += count
       element.innerHTML = `Total: ${counter}`
     }
-    element.addEventListener('click', () => setCounter(counter + 1))
-    setCounter(0)
+    document.addEventListener('click', () => {
+      counter =  0
+      const bits = document.querySelector('.binary').children
+      let i = 0
+      while(i < bits.length){
+        let bitVal = bits[i].firstChild.textContent 
+        if(bitVal == '1'){
+          addValue(parseInt(bits[i].dataset.value))
+        }
+        i++
+      }
+    })
+    addValue(0)
   }
   
