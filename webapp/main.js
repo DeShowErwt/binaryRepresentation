dda43eced8eb9843cee6e7aa92b53fe5a8443d3c
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-03-15 12:00:33 +0100

    #3 switching between bit and byte possible

--- webapp/main.js
+++ webapp/main.js
@@ -4,13 +4,17 @@ import { setupCounter } from './js/counter.js'
 
 setupCounter(document.querySelector('.counter'))
 let byteList = [1,2,4,8,16,32,64,128]
-for(let i = 0; i<byteList.length;i++){
-    
+let bitList = [1]
+function displayList(chosenList){
+    const binary = document.querySelector('.binary')
+    binary.innerHTML = ''
+    for(let i = 0; i<chosenList.length;i++){
+         binary.innerHTML += `
+            <binary-element data-value="${byteList[i]}"></binary-element>
+        `
+    }
 }
-
-document.querySelector('.binary').innerHTML += `
-    <binary-element data-value="1"></binary-element>
-    <binary-element data-value="2"></binary-element>
-    <binary-element data-value="4"></binary-element>
-    <binary-element data-value="8"></binary-element>
-`
+displayList(bitList)
+// Setup eventListeners for changing the size of the bytes
+document.querySelector('#bitSelect').addEventListener('click', () => {displayList(bitList)})
+document.querySelector('#byteSelect').addEventListener('click', () => {displayList(byteList)})
