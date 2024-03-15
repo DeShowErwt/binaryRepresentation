af6410aad5fa902ea35bc3d2f4acdb29a15ff022
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-03-15 13:02:19 +0100

    #4 Light up the legend number upon selection

--- webapp/js/binary.js
+++ webapp/js/binary.js
@@ -5,8 +5,15 @@ export class binaryElement extends HTMLElement {
   }
 
   _setBinary(target){
+    let legendClasses = target.querySelector('.legendNumber').classList
+    // For the correct color representation of the legendNumber use this toggle with the classname to be used if it is not
+    let toggle = 'legendNumberUntoggled'
     this.state = !this.state
-    target.innerHTML = `<span class="bit">${+ this.state}</span><span class="legendNumber">${target.dataset.value}</span>`
+    // If the state is true we want to show its toggled, also with the legendNumber
+    if(this.state){legendClasses.remove('legendNumberUntoggled');toggle = 'legendNumberToggled'}
+    else{legendClasses.remove('legendNumberToggled')}
+    target.querySelector('.bit').innerHTML = `${+ this.state}`
+    legendClasses.add(toggle)
   }
 
   connectedCallback(){
@@ -14,7 +21,8 @@ export class binaryElement extends HTMLElement {
   }
 
   render(){
-    this.innerHTML = `<span class="bit">${+ this.state}</span><span class="legendNumber">${this.dataset.value}</span>`
+    this.innerHTML = `<div class="bit">${+ this.state}</div><div class="legendNumber">${this.dataset.value}</div>`
+    this.querySelector('.legendNumber').classList.add('legendNumberUntoggled')
     this.addEventListener('click', () => this._setBinary(this))
   }
 }
