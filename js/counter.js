159279dd741de0da2ca7e8ded4d13b3a80482f4b
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-03-18 20:05:42 +0100

    Better comments

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,8 @@
 let decimalNum;
+// We use this one function to set up the counter and have it behave as the item we want
 export function setupCounter(element) {
   let counter = 0
+
   const addValue = (count) => {
     counter += count
     element.innerHTML = `Decimal number: <input id="changableVal" min="0" max="255" value="${counter}">`
@@ -11,20 +13,19 @@ export function setupCounter(element) {
       document.dispatchEvent(decimalToBinaryEvent);
     })
   }
+
   document.querySelector('.center').addEventListener('click', () => {
     counter =  0
     // Create a bool to check if anything has changed, if not, the counter won't be updated and 
     // therefore the previous value will still be displayed, we use this bool to update it in that case
     let empty = true
     const rows = document.getElementsByClassName('row')
-    console.log(rows)
     let bits = []
     for(let rowIndex = 0; rowIndex < rows.length; rowIndex++){
       for(let childIndex = 0; childIndex < rows[rowIndex].children.length; childIndex++){
         bits.push(rows[rowIndex].children[childIndex])
       }      
     }
-    console.log(bits)
     for(let bitIndex = 0; bitIndex < bits.length; bitIndex++){
       let bitVal = bits[bitIndex].firstChild.textContent 
       if(bitVal == '1'){
@@ -35,6 +36,7 @@ export function setupCounter(element) {
     // If every bit is switched off run the function to display 0
     if(empty){addValue(0)}
   })
-  // To init run the addValue function with 0
+  
+  // To init the counter run the addValue function with 0
   addValue(0)
 }
